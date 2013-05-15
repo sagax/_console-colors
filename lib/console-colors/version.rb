ffef0371810be7ea329bfb5debac811d982f8232
Mikhail Krainik <avastor.developer@gmail.com>
2013-05-15 18:31:37 +0300

    fixed version

--- lib/console-colors/version.rb
+++ lib/console-colors/version.rb
@@ -3,6 +3,6 @@ module Console
     MAJOR = 0
     MINOR = 0
     TINY = 2
-    VERSION = [MAJOR, MINOR, TINY, PRE].compact.join('.')
+    VERSION = [MAJOR, MINOR, TINY].compact.join('.')
   end
 end
